21546a7b07df0cae1edadb6c14b8f30c81c458a2
Imre Cserhati <cserhati.imre.andras@gmail.com>
2024-03-30 22:09:53 +0100

    created authentication page component alongside sign in form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -55,7 +55,7 @@ const SignUpForm = () => {
 
   return (
     <div className='sign-up-container'>
-      <h2>Don't have an account?</h2>
+      <h2>I do not have an account</h2>
       <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
@@ -94,7 +94,7 @@ const SignUpForm = () => {
           value={confirmPassword}
         />
 
-        <Button type='submit'>Sign Up</Button>
+        <Button type='submit'>SIGN UP</Button>
       </form>
     </div>
   );
