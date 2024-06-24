fd5c58d97754a64fd57c94cbc424d2a1b19125d0
Imre Cserhati <cserhati.imre.andras@gmail.com>
2024-06-24 23:43:05 +0200

    remove custom logger and use the one from redux-logger

--- src/store/store.js
+++ src/store/store.js
@@ -5,31 +5,25 @@ import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type:', action.type);
-  console.log('payload:', action.payload);
-  console.log('currentState:', store.getState());
-
-  next(action);
-
-  console.log('next state:', store.getState());
-};
-
 const persistConfig = {
   key: 'root',
   storage,
   blacklist: ['user'],
 };
 
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const middleWares = [loggerMiddleware];
-
-const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
